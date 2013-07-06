9c058bd34326e219d3a797946fc3c8f50ae72462
suhorng <suhorng@gmail.com>
2013-07-06 14:00:16 +0800

    rename files

--- runtime.cpp
+++ runtime.cpp
@@ -1,200 +1,99 @@
-#include <stdexcept>
-#include <string>
-#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cstdint>
 #include <memory>
-#include <tuple>
-#include <unordered_set> //used in show(Machine)
-#include <unordered_map>
-#include "runtime.h"
+#include <vector>
+#include <string>
+#include "vm.h"
 
 using namespace std;
 
-string show(ValueType type) {
-  switch (type) {
-  case ValueType::ClosureType:
-    return "ClosureType";
-  case ValueType::IntType:
-    return "IntType";
-  default:
-    throw logic_error("show: not valid ValueType");
-    return "unknown type";
+vector<code_t> get_input() {
+  int64_t c;
+  vector<code_t> res;
+  while (cin >> c) {
+    res.push_back(c);
   }
-}
-
-shared_ptr<Env> Nil() {
-  return shared_ptr<Env>(new EnvNil());
-}
-
-shared_ptr<Env> Cons(const shared_ptr<Value>& _car,
-                     const shared_ptr<Env>& _cdr)
-{
-  return shared_ptr<Env>(new EnvCons(_car, _cdr));
-}
-
-static shared_ptr<Value> env_access(shared_ptr<Env> env, uint64_t idx) {
-  while (env->type != EnvType::NilType) {
-    EnvCons* cons = dynamic_cast<EnvCons*>(env.get());
-    if (idx == 0) {
-      return cons->car;
-    }
-    env = cons->cdr;
-    idx = idx - 1;
-  }
-  throw runtime_error("Environment access index out of bound");
-  return shared_ptr<Value>(nullptr);
-}
-
-string show(const shared_ptr<Value>& val) {
-  if (val->type == ValueType::IntType) {
-    return to_string(dynamic_cast<IntValue&>(*val.get()).integer);
-  } else if (val->type == ValueType::ClosureType) {
-    return string("<function@")
-          + to_string(dynamic_cast<Closure&>(*val.get()).code_addr)
-          + ">";
-  } else {
-    return "(unknown type)";
-  }
-}
-
-string show(Machine& M) {
-  static const unordered_set<uint64_t> dbl_op =
-    {ACCESS, FUNCTION, CONSTINT, BRANCHNZ_REL, JUMP_REL};
-  static const unordered_map<uint64_t, string> disasm =
-    {{ACCESS, "ACCESS"}, {FUNCTION, "FUNCTION"}, {SAVE, "SAVE"}, {RESTORE, "RESTORE"},
-     {CALL, "CALL"}, {RETURN, "RETURN"}, {HALT, "HALT"}, {CONSTINT, "CONSTINT"},
-     {ADD, "ADD"}, {MUL, "MUL"}, {BRANCHNZ_REL, "BRANCHNZ_REL"}, {JUMP_REL, "JUMP_REL"}};
-  string res;
-  res += string("EIP: ") + to_string(M.eip);
-  res += string("    op code: ") + to_string(M.code[M.eip]);
-  if (dbl_op.count(M.code[M.eip]))
-    res += string(" ") + to_string(M.code[M.eip+1]);
-  res += string("   ") + disasm.at(M.code[M.eip]);
-  if (dbl_op.count(M.code[M.eip]))
-    res += string(" ") + to_string(M.code[M.eip+1]);
-  res += "\nvalues:";
-  for (auto it=M.values.rbegin(); it!=M.values.rend(); ++it) {
-    res += " ";
-    res += show(*it); 
-  }
-  res += "\nstk:";
-  for (auto it=M.stk.rbegin(); it!=M.stk.rend(); ++it) {
-    res += " ";
-    if (it->type == StackType::AddrType) {
-      res += "AddrType@";
-      res += to_string(it->addr);
-    } else {
-      res += "EnvType";
-    }
-  }
-  res += "\nenv:";
-  for (shared_ptr<Env> e=M.env; e->type!=EnvType::NilType;) {
-    res += " " + show(dynamic_cast<EnvCons&>(*e).car);
-    e = dynamic_cast<EnvCons&>(*e).cdr;
-  }
-  res += "\n";
   return move(res);
 }
 
-shared_ptr<Value> run(const code_t *codes, size_t _code_len) {
-  using VT = ValueType;
-  Machine M {0, _code_len, codes, {}, {}, Nil()};
-  for (;;) {
-//cout<<show(M)<<endl;
-    Code op = M.fetch<Code>();
-    switch (op) {
+vector<string> disassemble(const vector<code_t>& code) {
+  vector<string> res;
+  for (auto it=code.begin(); it<code.end(); ++it) {
+    switch (*it) {
     case ACCESS:
-      {
-        uint64_t idx = M.fetch<uint64_t>();
-        M.values.push_back(env_access(M.env, idx));
-      }
+      ++it;
+      res.push_back(to_string(it-code.begin()-1) + "\taccess " + to_string(*it));
       break;
     case FUNCTION:
-      {
-        addr_t addr = M.fetch<addr_t>();
-        M.values.emplace_back(new Closure(addr, M.env));
-      }
+      ++it;
+      res.push_back(to_string(it-code.begin()-1) + "\tfunction " + to_string(*it));
       break;
     case SAVE:
-      M.stk.emplace_back(M.env);
+      res.push_back(to_string(it-code.begin()) + "\tsave");
       break;
     case RESTORE:
-      if (M.stk.empty() || M.stk.back().type!=StackType::EnvType)
-        throw runtime_error("Expected saved environment at stack top");
-      M.env = M.stk.back().env;
-      M.stk.pop_back();
+      res.push_back(to_string(it-code.begin()) + "\trestore");
       break;
     case CALL:
+      res.push_back(to_string(it-code.begin()) + "\tcall");
+      break;
     case TAILCALL:
-      {
-        shared_ptr<Value> closure_val = M.get_value(VT::ClosureType);
-        Closure& closure = dynamic_cast<Closure&>(*closure_val);
-        M.values.pop_back();
-        shared_ptr<Value> val = M.get_value();
-        M.values.pop_back();
-
-        if (op == CALL) {
-          M.stk.emplace_back(M.eip);
-        }
-        M.env = Cons(val, closure.env);
-        M.eip = closure.code_addr;
-      }
+      res.push_back(to_string(it-code.begin()) + "\ttailcall");
       break;
     case RETURN:
-      if (M.stk.back().type != StackType::AddrType)
-        throw runtime_error("Expected return address at stack top");
-      M.eip = M.stk.back().addr;
-      M.stk.pop_back();
+      res.push_back(to_string(it-code.begin()) + "\treturn");
       break;
     case HALT:
-      return M.values.empty()? shared_ptr<Value>(nullptr) : M.values.back();
+      res.push_back(to_string(it-code.begin()) + "\thalt");
+      break;
     case CONSTINT:
-      {
-        int integer = M.fetch<int>();
-        M.values.emplace_back(new IntValue(integer));
-      }
+      ++it;
+      res.push_back(to_string(it-code.begin()-1) + "\tconstint " + to_string(static_cast<int64_t>(*it)));
       break;
     case ADD:
-      {
-        int val2 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
-        M.values.pop_back();
-
-        int val1 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
-        M.values.pop_back();
-
-        M.values.emplace_back(new IntValue(val1 + val2));
-      }
-      break;
-    case MUL:
-      {
-        int val2 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
-        M.values.pop_back();
-
-        int val1 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
-        M.values.pop_back();
-
-        M.values.emplace_back(new IntValue(val1 * val2));
-      }
+      res.push_back(to_string(it-code.begin()) + "\tadd");
       break;
     case BRANCHNZ_REL:
-      {
-        diff_t rel = M.fetch<diff_t>();
-
-        int val = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
-        M.values.pop_back();
-
-        if (val != 0) {
-          M.eip = M.eip + rel;
-        }
-      }
+      ++it;
+      res.push_back(to_string(it-code.begin()-1) + "\tjnz_rel +" + to_string(*it));
       break;
     case JUMP_REL:
-      {
-        diff_t rel = M.fetch<diff_t>();
-        M.eip = M.eip + rel;
-      }
+      ++it;
+      res.push_back(to_string(it-code.begin()-1) + "\tjmp_rel +" + to_string(*it));
       break;
     default:
-      throw runtime_error(string("Undefined OP Code: ") + to_string(op));
+      res.push_back(to_string(it-code.begin()) + "\tUnrecognized op-code: " + to_string(*it));
     }
   }
+  return move(res);
+}
+
+int main() {
+  try {
+    vector<code_t> code(get_input());
+    vector<string> disasm(disassemble(code));
+    for (string& str : disasm) {
+      cout << str << endl;
+    }
+    shared_ptr<Value> res = run(code.data(), code.size());
+    if (not res) {
+      cout << "result is null" << endl;
+    } else {
+      cout << show(res->type) << ": ";
+      switch (res->type) {
+      case ValueType::IntType:
+        cout << dynamic_cast<IntValue*>(res.get())->integer << endl;
+        break;
+      case ValueType::ClosureType:
+        cout << "<function>" << endl;
+        break;
+      default:
+        cout << "unknown value type" << endl;
+      }
+    }
+  } catch (const exception& e) {
+    cout << "***Exception: " << e.what() << endl;
+  }
+  return 0;
 }
--- vm.cpp
+++ vm.cpp
@@ -1,99 +1,200 @@
-#include <algorithm>
-#include <iostream>
-#include <cstdint>
-#include <memory>
-#include <vector>
+#include <stdexcept>
 #include <string>
-#include "runtime.h"
+#include <vector>
+#include <memory>
+#include <tuple>
+#include <unordered_set> //used in show(Machine)
+#include <unordered_map>
+#include "vm.h"
 
 using namespace std;
 
-vector<code_t> get_input() {
-  int64_t c;
-  vector<code_t> res;
-  while (cin >> c) {
-    res.push_back(c);
+string show(ValueType type) {
+  switch (type) {
+  case ValueType::ClosureType:
+    return "ClosureType";
+  case ValueType::IntType:
+    return "IntType";
+  default:
+    throw logic_error("show: not valid ValueType");
+    return "unknown type";
   }
+}
+
+shared_ptr<Env> Nil() {
+  return shared_ptr<Env>(new EnvNil());
+}
+
+shared_ptr<Env> Cons(const shared_ptr<Value>& _car,
+                     const shared_ptr<Env>& _cdr)
+{
+  return shared_ptr<Env>(new EnvCons(_car, _cdr));
+}
+
+static shared_ptr<Value> env_access(shared_ptr<Env> env, uint64_t idx) {
+  while (env->type != EnvType::NilType) {
+    EnvCons* cons = dynamic_cast<EnvCons*>(env.get());
+    if (idx == 0) {
+      return cons->car;
+    }
+    env = cons->cdr;
+    idx = idx - 1;
+  }
+  throw runtime_error("Environment access index out of bound");
+  return shared_ptr<Value>(nullptr);
+}
+
+string show(const shared_ptr<Value>& val) {
+  if (val->type == ValueType::IntType) {
+    return to_string(dynamic_cast<IntValue&>(*val.get()).integer);
+  } else if (val->type == ValueType::ClosureType) {
+    return string("<function@")
+          + to_string(dynamic_cast<Closure&>(*val.get()).code_addr)
+          + ">";
+  } else {
+    return "(unknown type)";
+  }
+}
+
+string show(Machine& M) {
+  static const unordered_set<uint64_t> dbl_op =
+    {ACCESS, FUNCTION, CONSTINT, BRANCHNZ_REL, JUMP_REL};
+  static const unordered_map<uint64_t, string> disasm =
+    {{ACCESS, "ACCESS"}, {FUNCTION, "FUNCTION"}, {SAVE, "SAVE"}, {RESTORE, "RESTORE"},
+     {CALL, "CALL"}, {RETURN, "RETURN"}, {HALT, "HALT"}, {CONSTINT, "CONSTINT"},
+     {ADD, "ADD"}, {MUL, "MUL"}, {BRANCHNZ_REL, "BRANCHNZ_REL"}, {JUMP_REL, "JUMP_REL"}};
+  string res;
+  res += string("EIP: ") + to_string(M.eip);
+  res += string("    op code: ") + to_string(M.code[M.eip]);
+  if (dbl_op.count(M.code[M.eip]))
+    res += string(" ") + to_string(M.code[M.eip+1]);
+  res += string("   ") + disasm.at(M.code[M.eip]);
+  if (dbl_op.count(M.code[M.eip]))
+    res += string(" ") + to_string(M.code[M.eip+1]);
+  res += "\nvalues:";
+  for (auto it=M.values.rbegin(); it!=M.values.rend(); ++it) {
+    res += " ";
+    res += show(*it); 
+  }
+  res += "\nstk:";
+  for (auto it=M.stk.rbegin(); it!=M.stk.rend(); ++it) {
+    res += " ";
+    if (it->type == StackType::AddrType) {
+      res += "AddrType@";
+      res += to_string(it->addr);
+    } else {
+      res += "EnvType";
+    }
+  }
+  res += "\nenv:";
+  for (shared_ptr<Env> e=M.env; e->type!=EnvType::NilType;) {
+    res += " " + show(dynamic_cast<EnvCons&>(*e).car);
+    e = dynamic_cast<EnvCons&>(*e).cdr;
+  }
+  res += "\n";
   return move(res);
 }
 
-vector<string> disassemble(const vector<code_t>& code) {
-  vector<string> res;
-  for (auto it=code.begin(); it<code.end(); ++it) {
-    switch (*it) {
+shared_ptr<Value> run(const code_t *codes, size_t _code_len) {
+  using VT = ValueType;
+  Machine M {0, _code_len, codes, {}, {}, Nil()};
+  for (;;) {
+//cout<<show(M)<<endl;
+    Code op = M.fetch<Code>();
+    switch (op) {
     case ACCESS:
-      ++it;
-      res.push_back(to_string(it-code.begin()-1) + "\taccess " + to_string(*it));
+      {
+        uint64_t idx = M.fetch<uint64_t>();
+        M.values.push_back(env_access(M.env, idx));
+      }
       break;
     case FUNCTION:
-      ++it;
-      res.push_back(to_string(it-code.begin()-1) + "\tfunction " + to_string(*it));
+      {
+        addr_t addr = M.fetch<addr_t>();
+        M.values.emplace_back(new Closure(addr, M.env));
+      }
       break;
     case SAVE:
-      res.push_back(to_string(it-code.begin()) + "\tsave");
+      M.stk.emplace_back(M.env);
       break;
     case RESTORE:
-      res.push_back(to_string(it-code.begin()) + "\trestore");
+      if (M.stk.empty() || M.stk.back().type!=StackType::EnvType)
+        throw runtime_error("Expected saved environment at stack top");
+      M.env = M.stk.back().env;
+      M.stk.pop_back();
       break;
     case CALL:
-      res.push_back(to_string(it-code.begin()) + "\tcall");
-      break;
     case TAILCALL:
-      res.push_back(to_string(it-code.begin()) + "\ttailcall");
+      {
+        shared_ptr<Value> closure_val = M.get_value(VT::ClosureType);
+        Closure& closure = dynamic_cast<Closure&>(*closure_val);
+        M.values.pop_back();
+        shared_ptr<Value> val = M.get_value();
+        M.values.pop_back();
+
+        if (op == CALL) {
+          M.stk.emplace_back(M.eip);
+        }
+        M.env = Cons(val, closure.env);
+        M.eip = closure.code_addr;
+      }
       break;
     case RETURN:
-      res.push_back(to_string(it-code.begin()) + "\treturn");
+      if (M.stk.back().type != StackType::AddrType)
+        throw runtime_error("Expected return address at stack top");
+      M.eip = M.stk.back().addr;
+      M.stk.pop_back();
       break;
     case HALT:
-      res.push_back(to_string(it-code.begin()) + "\thalt");
-      break;
+      return M.values.empty()? shared_ptr<Value>(nullptr) : M.values.back();
     case CONSTINT:
-      ++it;
-      res.push_back(to_string(it-code.begin()-1) + "\tconstint " + to_string(static_cast<int64_t>(*it)));
+      {
+        int integer = M.fetch<int>();
+        M.values.emplace_back(new IntValue(integer));
+      }
       break;
     case ADD:
-      res.push_back(to_string(it-code.begin()) + "\tadd");
+      {
+        int val2 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
+        M.values.pop_back();
+
+        int val1 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
+        M.values.pop_back();
+
+        M.values.emplace_back(new IntValue(val1 + val2));
+      }
+      break;
+    case MUL:
+      {
+        int val2 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
+        M.values.pop_back();
+
+        int val1 = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
+        M.values.pop_back();
+
+        M.values.emplace_back(new IntValue(val1 * val2));
+      }
       break;
     case BRANCHNZ_REL:
-      ++it;
-      res.push_back(to_string(it-code.begin()-1) + "\tjnz_rel +" + to_string(*it));
+      {
+        diff_t rel = M.fetch<diff_t>();
+
+        int val = dynamic_cast<IntValue&>(*M.get_value(VT::IntType)).integer;
+        M.values.pop_back();
+
+        if (val != 0) {
+          M.eip = M.eip + rel;
+        }
+      }
       break;
     case JUMP_REL:
-      ++it;
-      res.push_back(to_string(it-code.begin()-1) + "\tjmp_rel +" + to_string(*it));
+      {
+        diff_t rel = M.fetch<diff_t>();
+        M.eip = M.eip + rel;
+      }
       break;
     default:
-      res.push_back(to_string(it-code.begin()) + "\tUnrecognized op-code: " + to_string(*it));
+      throw runtime_error(string("Undefined OP Code: ") + to_string(op));
     }
   }
-  return move(res);
-}
-
-int main() {
-  try {
-    vector<code_t> code(get_input());
-    vector<string> disasm(disassemble(code));
-    for (string& str : disasm) {
-      cout << str << endl;
-    }
-    shared_ptr<Value> res = run(code.data(), code.size());
-    if (not res) {
-      cout << "result is null" << endl;
-    } else {
-      cout << show(res->type) << ": ";
-      switch (res->type) {
-      case ValueType::IntType:
-        cout << dynamic_cast<IntValue*>(res.get())->integer << endl;
-        break;
-      case ValueType::ClosureType:
-        cout << "<function>" << endl;
-        break;
-      default:
-        cout << "unknown value type" << endl;
-      }
-    }
-  } catch (const exception& e) {
-    cout << "***Exception: " << e.what() << endl;
-  }
-  return 0;
 }
